e355566487ea00795932207a80ee2b65fcf8fa50
Jim Owens <jimcowens@gmail.com>
2018-12-13 17:44:03 -0500

    working on deck looking nice

--- Thursday-12-13/blackjack/scripts.js
+++ Thursday-12-13/blackjack/scripts.js
@@ -2,19 +2,41 @@ const freshDeck = createDeck()
 let theDeck = freshDeck.slice()
 let playerHand = []
 let dealerHand = []
+let hasAce=false
+let topCard = []
+let stay = false
 
 $('.deal-button').click(()=>{
-    console.log('click')
+    // console.log('click')
     // console.log("User clicked the deal button")
     // we need a deck!
     shuffleDeck(theDeck)
     // shift takes pulls the first card off the array and returns it.
-    console.log(theDeck)
-    let topCard = theDeck.shift()
-    console.log(topCard)
-    console.log(theDeck)
-
+    topCard = theDeck.shift()
+    dealerHand.push(topCard)
+    topCard=theDeck.shift()
+    playerHand.push(topCard)
+    topCard=theDeck.shift()
+    dealerHand.push(topCard)
+    topCard=theDeck.shift()
+    playerHand.push(topCard)
+    placeCard(`player`,1,playerHand[0])
+    placeCard(`dealer`,1,dealerHand[0])
+    placeCard(`player`,2,playerHand[1])
+    placeCard(`dealer`,2,dealerHand[1])
+    calculateTotal(playerHand, 'player')
+    calculateTotal(dealerHand, 'dealer')
+    $('.deal-button').css(`display`, `none`)
 })
+
+function placeCard(who, where, what){
+    // who got it, 
+    // who got it, -  option player or dealer
+    // where is it in the hand - option 1-6
+    // what card was it - 1-13h, 1-13s, etc...
+    const classSelector = `.${who}-cards .card-${where}`
+    $(classSelector).html(`<img src="/cards/${what}.png" />`)
+}
 
 function createDeck(){
     let newDeck = []
@@ -23,10 +45,9 @@ function createDeck(){
     // that function gets 2 args:
     // 1.what to call this element in loop
     // 2. index loop is on
-
     // outer loop is for suits, inner is for number
     const suits = ['s','h','d','c']
-    suits.forEach((s,index)=>{
+    suits.forEach((s,i)=>{
         for(let c=1; c<=13;c++){
             newDeck.push(`${c}${s}`)
         }
@@ -34,17 +55,73 @@ function createDeck(){
     return newDeck
 }
 
-function shuffleDeck(aDeckToBeShuffled) {
+function shuffleDeck(deckToBeShuffled) {
     // loop. A lot. Like those machines in casinos that 
     // make funny noises.
     for (let i=0; i<100000; i++){
         let rand1 = Math.floor(Math.random()*52)
         let rand2 = Math.floor(Math.random()*52)
-        // we need to switch aDeckToBeShuffled[rand1] with aDeckToBeShuffled[rand2].
+        // we need to switch deckToBeShuffled[rand1] with deckToBeShuffled[rand2].
         // But we have to save the value of the one to keep it for later
-        let cardDefender = aDeckToBeShuffled[rand1]
-        aDeckToBeShuffled[rand1]=aDeckToBeShuffled[rand2]
-        aDeckToBeShuffled[rand2]= cardDefender 
+        let cardHolder = deckToBeShuffled[rand1]
+        deckToBeShuffled[rand1]=deckToBeShuffled[rand2]
+        deckToBeShuffled[rand2]= cardHolder 
     }
-    console.log(aDeckToBeShuffled)
 }
+function calculateTotal(hand, who){
+    // purpose: findout the humber, and return it
+    let handTotal = 0
+    let cardValues = []
+    let hasAce = false
+    hand.forEach((card,i)=>{
+        let thisCardValue = card.slice(0,-1)
+        console.log(`the card length for this ${card} is ${card.length}`)
+        if(card.length>2){
+            // handles JQK
+            thisCardValue = 10
+        }
+        if(thisCardValue == '1'){
+            thisCardValue = 11
+            hasAce = true
+        }
+        handTotal+= Number(thisCardValue)
+        cardValues.push(thisCardValue)
+    })
+    console.log(handTotal)
+    if (who =='player'){
+    $('.playerTotal').html(`${handTotal}`)
+    } else{
+    $(`.dealerTotal`).html(`${handTotal}`)
+    }
+    return handTotal
+}
+
+$(`.hit-button`).click(() =>{
+    const topCard = theDeck.shift()
+    playerHand.push(topCard)
+    placeCard(`player`,playerHand.length,topCard)
+    calculateTotal(playerHand,'player')
+    if (stay == true){
+        $('.hit-button').css(`display`, `none`)
+    }
+})
+$(`.stay-button`).click(() =>{
+    let dealersTotal = calculateTotal(dealerHand, `dealer`)
+    console.log(`dealersTotal is ${dealersTotal}`)
+    while (dealersTotal<17&&hasAce==false){
+        topCard=theDeck.shift()
+        dealerHand.push(topCard)
+        placeCard(`dealer`,dealerHand.length, topCard)
+        dealersTotal = calculateTotal(dealerHand, `dealer`)
+        stay = true
+        $('.stay-button').css(`display`, `none`)
+    }
+})
+
+// function checkLoss(){
+//     (if dealersTotal>21){
+//     player wins
+// }
+//     if (playerTotal)
+
+// }
